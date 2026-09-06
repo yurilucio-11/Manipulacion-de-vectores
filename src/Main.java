ac980ecbf1dc29e0dc56377c483354e9d24c0b64
ma4rc312 <lucioyurimarcela03@gmail.com>
2026-09-06 11:30:18 -0500

    feat: agregar busqueda, valores mayor y menor

--- src/Main.java
+++ src/Main.java
@@ -34,10 +34,63 @@ public class Main {
         System.out.println("VCETOR INGRESADO");
         System.out.println("=========================");
 
-        for (int i = 0 ; i < vector.length; i++) {
-            System.out.println("Posicion" + i + ": " + vector[i]);
-        }
+for (int i = 0; i < vector.length; i++) {
+    System.out.println("Posicion " + i + ": " + vector[i]);
+}
 
-        scanner.close();
+System.out.println("\n==========================================");
+System.out.println("BUSCAR UN VALOR");
+System.out.println("==========================================");
+
+System.out.print("Ingrese el numero que desea buscar: ");
+int numeroBuscar = scanner.nextInt();
+
+boolean encontrado = false;
+
+for (int i = 0; i < vector.length; i++) {
+
+    if (vector[i] == numeroBuscar) {
+
+        System.out.println(
+            "El numero " + numeroBuscar
+            + " se encuentra en la posicion " + i + "."
+        );
+
+        encontrado = true;
+    }
+}
+
+if (!encontrado) {
+
+    System.out.println(
+        "El numero " + numeroBuscar
+        + " no se encuentra en el vector."
+    );
+}
+
+int mayor = vector[0];
+int menor = vector[0];
+
+for (int i = 1; i < vector.length; i++) {
+
+    if (vector[i] > mayor) {
+        mayor = vector[i];
+    }
+
+    if (vector[i] < menor) {
+        menor = vector[i];
+    }
+}
+
+System.out.println("\n==========================================");
+System.out.println("MAYOR Y MENOR VALOR");
+System.out.println("==========================================");
+
+System.out.println("Mayor valor: " + mayor);
+System.out.println("Menor valor: " + menor);
+
+
+scanner.close();
+    
     }
 }
